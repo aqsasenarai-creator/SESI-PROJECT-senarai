6c24e32cc65e012ffb7a58dc5e5645ab5a380caf
aqsasenarai-creator <aqsasenarai@gmail.com>
2025-09-30 14:33:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ const tombolSapa = document.getElementById('Sapa');
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Makasih bang!');
 });
+
+const toggleBtn = document.getElementById('toggle-mode');
+const body = document.body;
+
+// Cek apakah user sudah punya preferensi
+if (localStorage.getItem('mode') === 'dark') {
+  body.classList.add('dark-mode');
+}
+
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  // Simpan preferensi ke localStorage
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('mode', 'dark');
+  } else {
+    localStorage.setItem('mode', 'light');
+  }
+});
